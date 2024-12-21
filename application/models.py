bb508e5178fc03d047fcc585e51e9acf40f19119
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-22 01:12:20 +0300

    python manage.py makemigrations

--- application/models.py
+++ application/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class User(models.Model):
     age = models.IntegerField()
     name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
