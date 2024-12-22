9e23c2ce8dfea75b0af0a3fd57ddce2ba7f1a628
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-22 14:59:43 +0300

    returning all data in the database

--- application/views.py
+++ application/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.serializers import Serializer
+
 from .models import User
 from .serializer import UserSerializer
 
@@ -10,8 +12,10 @@ from .serializer import UserSerializer
 @api_view(['GET'])
 # defining endpoint function
 
-def get_user(request):
-    return Response(UserSerializer({'name': "Cyrus", "age": 34}).data)
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(Serializer.data)
 
 @api_view(['POST'])
 def create_user(request):
