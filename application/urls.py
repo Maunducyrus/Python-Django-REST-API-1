713befc46a09f7d6d7e953665e9783b1a2479b48
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-22 17:04:36 +0300

    import user detail function

--- application/urls.py
+++ application/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import get_users, create_user
+from .views import get_users, create_user, user_detail
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
