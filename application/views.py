827f1dff18002785ceadb00c71893e79d5a63331
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-12-22 16:37:19 +0300

    updates on the API project

--- application/views.py
+++ application/views.py
@@ -1,10 +1,7 @@
-from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.serializers import Serializer
-
 from .models import User
 from .serializer import UserSerializer
 
@@ -29,3 +26,23 @@ def create_user(request):
 
 # adding more methods
 @api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
